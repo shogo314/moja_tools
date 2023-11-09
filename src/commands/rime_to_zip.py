2b2feed4e2de3a21863fc8dbff270d82d346ba95
shogo314 <shg.kawasome@gmail.com>
2023-11-09 20:50:06 +0900

    fix

--- src/commands/rime_to_zip.py
+++ src/commands/rime_to_zip.py
@@ -173,6 +173,11 @@ def main(argv):
     write_testcases(rime_out_testset, rime_out_solution, moja_out_in, moja_out_out)
 
     problem_slug = problem_directory_path.split(os.path.sep)[-1]
-    shutil.make_archive(problem_slug, format="zip", root_dir=problem_directory_path)
+    shutil.make_archive(
+        base_name=problem_slug,
+        format="zip",
+        root_dir=moja_out_directory_path,
+        base_dir=problem_directory_path,
+    )
 
     return 0
